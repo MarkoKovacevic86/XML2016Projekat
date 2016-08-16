aa6c9867f1d051bb62d063951afdeff08395ab06
Stefa <stefancina@live.com>
2016-08-16 18:32:35 +0200

    Kreiran login controller i loginResource

--- XMLProject/WebContent/index.js
+++ XMLProject/WebContent/index.js
@@ -1,33 +1,29 @@
 (function(angular) {
 
-	var xml = angular.module('xml', [ 'ngRoute', 'ui.router','ngStorage'])
-			
-			
-	xml.config(config)
-	.run(run)
-	function config($stateProvider){
-		//$urlRouteProvider.otherwise('#/');
-		$stateProvider
-		.state('/#',{
-			templateUrl : 'index.html',
-			//controller : ''
-		})
-		.state('login',{
-			url:'/login',
-			templateUrl:'client/login/login.html'
-			//controller : ''
+	var xml = angular.module('xml', [ 'ngRoute', 'ui.router', 'ngStorage' ])
+
+	xml.config(config).run(run)
+	function config($stateProvider) {
+		// $urlRouteProvider.otherwise('#/');
+		$stateProvider.state('/#', {
+			templateUrl : 'index.html'
+		}).state('login', {
+			url : '/login',
+			templateUrl : 'client/login/login.html',
+			controller : 'loginCtrl'
 		})
 	}
-	
-	function run($rootScope, $http, $location,$localStorage,$state){
-		$rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-	          // lista javnih stanja
-	          var publicStates = ['login','signup',/*'entry',*/''];
-	          var restrictedState = publicStates.indexOf(toState.name) === -1;
-	          if(restrictedState && !AuthenticationService.getCurrentUser()){
-	            $state.go('login');
-	          }
-	        });
+
+	function run($rootScope, $http, $location, $localStorage, $state) {
+		$rootScope.$on('$stateChangeSuccess', function(event, toState,
+				toParams, fromState, fromParams) {
+			// lista javnih stanja
+			var publicStates = [ 'login', 'signup',/* 'entry', */'' ];
+			var restrictedState = publicStates.indexOf(toState.name) === -1;
+			if (restrictedState && !AuthenticationService.getCurrentUser()) {
+				$state.go('login');
+			}
+		});
 	}
-	
+
 }(angular))
