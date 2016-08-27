4a92c15da8fba1bebb9f312146f6bc6f1b666694
Stefan Popov Plavšić <stefancina@live.com>
2016-08-27 09:34:32 +0200

    Odradjen Login preko Genson-a

--- XMLProject/src/rest/LoginService.java
+++ XMLProject/src/rest/LoginService.java
@@ -5,12 +5,17 @@ import java.io.File;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.owlike.genson.Genson;
+import com.owlike.genson.GensonBuilder;
 
 import sun.awt.datatransfer.DataTransferer;
 import uloge.User;
@@ -22,46 +27,38 @@ public class LoginService {
 
 	@Path("/login")
 	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	public User login(String json) {
-		System.out.println(json);
-		ObjectMapper mapper = new ObjectMapper();
-		User u = null;
+	@Consumes(MediaType.APPLICATION_JSON)
+	public User log(String user){
+	
+		Genson genson = new GensonBuilder().useDateFormat(new SimpleDateFormat("yyyy-MM-dd")).create();
+		User u = genson.deserialize(user, User.class);
+		
+		File f = new File(path);
 		try {
-			u = mapper.readValue(json, User.class);
-			System.out.println("__" + u.getUsername());
-			File f = new File(path);
-			try {
-
-				Scanner scanner = new Scanner(f);
-				while (scanner.hasNext()) {
-					String line = scanner.nextLine();
-					String[] splitStr = line.split(";");
-					String username = splitStr[0].trim();
-					String password = splitStr[1].trim();
-					String role = splitStr[2].trim();
-					if (username.equals(u.getUsername())) {
-						if (password.equals(u.getPassword())) {
-							scanner.close();
-							u.setRole(role);
-							return u;
-						} else {
-							scanner.close();
-							return null;
-						}
+			Scanner scanner = new Scanner(f);
+			while (scanner.hasNext()) {
+				String line = scanner.nextLine();
+				String[] splitStr = line.split(";");
+				String username = splitStr[0].trim();
+				String password = splitStr[1].trim();
+				String role = splitStr[2].trim();
+				if (username.equals(u.getUsername())) {
+					if (password.equals(u.getPassword())) {
+						scanner.close();
+						u.setRole(role);
+						return u;
+					} else {
+						scanner.close();
+						return null;
 					}
 				}
-
-			} catch (IOException e) {
-				e.printStackTrace();
 			}
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-
+		
 		return null;
-
 	}
 
 }
