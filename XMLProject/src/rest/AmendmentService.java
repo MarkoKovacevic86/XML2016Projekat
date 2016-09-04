57d97d8e2603d9e1731beebefda167d8ca990bde
Stefa <stefancina@live.com>
2016-09-04 14:53:34 +0200

    Gotova podloga za rest amandmana

--- XMLProject/src/rest/AmendmentService.java
+++ XMLProject/src/rest/AmendmentService.java
@@ -1,8 +1,12 @@
 package rest;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -11,15 +15,32 @@ import jaxB.amandman.Amandman;
 @Path("/amendments")
 public class AmendmentService {
 
-	
-	
+	@GET
+	@Path("/actAmendments/{id}")
+	public String getAmendments(@PathParam("id") String id) {
+		return id;
+	}
 
-	
+	@DELETE
+	@Path("/deleteAmendment/id")
+	public void removeAmendment(@PathParam("amId") String amId) {
+
+	}
+
 	@POST
 	@Path("/suggestAmendment")
 	@Consumes(MediaType.APPLICATION_XML)
 	public Response add(Amandman amandman) {
 		System.out.println("Usao u predlozi amandman");
+		return null;
+	}
+
+	@POST
+	@Path("/update/{status}")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_XML)
+	public Amandman changeStatus(Amandman amandman, @PathParam("status") String status) {
+
 		return null;
 	}
 
